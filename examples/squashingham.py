40ca965651ef60423914d5554b412daf14ff2c6d
Graham <4gra@users.noreply.github.com>
2019-12-02 22:14:28 +0000

    stop relying on palette

--- examples/squashingham.py
+++ examples/squashingham.py
@@ -7,7 +7,7 @@ import random
 from time import sleep
 
 
-class Palette(dict):
+class Score(dict):
     # hard-coded palette, ordered to be pretty
     palette = {
         (0, 0): (0, 0),  #
@@ -28,69 +28,16 @@ class Palette(dict):
         #    (8,8) : (0,0),
     }
 
-    selected = None
-    held = 0
-    painted = False
-    blink = False
-    BLINK_TIME = 15
-    swaps = None
-
-    def __init__(self, lp):
-        self.blink = self.BLINK_TIME
-        self.update(self.palette)
-        self.lp = lp
-        self.paint()
-        super(Palette, self).__init__()
-
-    # def swap(self):
-    #     self.swaps += [self.selected]
-
-    def paint(self, blink=None):
-        if not self.painted:
-            i = 0
-            for k, v in self.palette.items():
-                i += 1
-                (x, y) = k
-                (r, g) = v
-                self.lp.LedCtrlXY(x, y, r, g)
-            self.painted = True
-        if self.selected and self.selected != (0, 0) and self.selected != self.painted:
-            if self.held:
-                self.held += 1
-            (x, y) = self.selected
-            # if self.held > 50:
-            #    self[(self.selected)] = (0,0)
-            if blink is not None:
-                self.blink = blink
-            if self.blink > 0:
-                (r, g) = self[self.selected]
-                self.blink -= 1
-            else:
-                (r, g) = tweak(self[self.selected])
-                self.blink = self.BLINK_TIME
-                print(" (blink %d)" % self.held)
-            self.lp.LedCtrlXY(x, y, r, g)
-
-    def unselect(self, x, y):
-        self.held = 0
-        self.paint(10)
-        return self[(x, y)]
-
-    def select(self, x, y):
-        self.selected = (x, y)
-        self.held = 1
-        self.paint(5)
-        return self[self.selected]
-
-
-class Score(Palette):
     empty = Colour(0, 0)
     point = Colour(0, 3)
 
     def __init__(self, lp):
         self.count = 0
         self.lastcount = 0
-        super(Score, self).__init__(lp)
+        self.update(self.palette)
+        self.lp = lp
+        self.paint()
+        super(Score, self).__init__()
 
     def paint(self, blink=False):
         if self.lastcount != self.count:
@@ -138,7 +85,7 @@ def game_loop():
         mole = Colour(3, 0)
         hill = Colour(1, 0)
         easy_loc = (8, 8)
-        mpos = (None, None)  # mole position
+        mole_pos = (None, None)  # mole position
         squashed = False
         easy = None
         ticks = 0  # since last squash
@@ -146,15 +93,15 @@ def game_loop():
         while True:
             score.paint()
             # make a mole
-            mchance = (random.choice(range(100 - ticks)) < 1)
-            if mchance:
+            mole_chance = (random.choice(range(100 - ticks)) < 1)
+            if mole_chance:
                 ticks = 0
-                if mpos[0] is not None:
-                    lp.LedCtrlXY(*mpos, hill.r, hill.g)
-                print("Mole at %s, %s!" % mpos)
-                mpos = (random.choice(range(8)),
+                if mole_pos[0] is not None:
+                    lp.LedCtrlXY(*mole_pos, hill.r, hill.g)
+                print("Mole at %s, %s!" % mole_pos)
+                mole_pos = (random.choice(range(8)),
                         random.choice(range(1, 9)))
-                lp.LedCtrlXY(*mpos, mole.r, mole.g)
+                lp.LedCtrlXY(*mole_pos, mole.r, mole.g)
                 if squashed:
                     squashed = False
                 else:
@@ -186,7 +133,7 @@ def game_loop():
                         lp.LedCtrlXY(*easy_loc, *colour)
                     # handle squashing or missing
                     elif pressed and (x, y) not in score:
-                        if (x, y) == mpos:
+                        if (x, y) == mole_pos:
                             lp.LedCtrlXY(x, y, squash.r, squash.g)
                             if not squashed:
                                 score.inc()
